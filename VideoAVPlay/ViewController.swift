ed6c1c35752aa4e87bcf43a84e78a422df4b312e
Sh1mj1 <7wlgns@gmail.com>
2022-05-31 18:13:39 +0900

    Initial Commit

---
+++ VideoAVPlay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoAVPlay
+//
+//  Created by 심지훈 on 2022/05/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
